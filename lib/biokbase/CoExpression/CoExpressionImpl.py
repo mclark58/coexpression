b8a8ae6d27d5ed3f592c05f080a173614a9e5c9d
Shinjae Yoo <sjyoo@bnl.gov>
2017-01-26 07:02:38 +0000

    Disable report

--- lib/biokbase/CoExpression/CoExpressionImpl.py
+++ lib/biokbase/CoExpression/CoExpressionImpl.py
@@ -583,8 +583,9 @@ class CoExpression:
                                         ]
                                         })[0]
 
-        result = { "report_name" : reportName,"report_ref" : "{0}/{1}/{2}".format(report_info[6],report_info[0],report_info[4]) }
+        #result = { "report_name" : reportName,"report_ref" : "{0}/{1}/{2}".format(report_info[6],report_info[0],report_info[4]) }
         #result = {'workspace_name' : param['workspace_name'], 'out_object_name' : param['out_object_name']}
+        result = {'workspace' : param['workspace_name'], 'output' : param['out_object_name']}
         #END const_coex_net_clust
 
         # At some point might do deeper type checking...
